f825fd12fc4e5c66acbe1a4e314c39f83024b2da
Orkanen <Filip.v.antonsson@gmail.com>
2022-01-12 05:50:19 +0100

    tes
    test

--- test/test.js
+++ test/test.js
@@ -12,7 +12,37 @@ var should = chai.should();  // Using Should style
 var xToken = "mockTest";
 
 /* eslint-disable no-unused-vars, no-undef */
+describe('Test for failed', function() {
 
+    describe('Fail logging in to the home-page', function() {
+        it('should return 200 from GET /', function(done) {
+            request(server)
+                .post('/')
+                .end((err, res) => {
+                    res.status.should.be.equal(302);
+                    //res.text.should.be.equal("Found. Redirecting to /map");
+                    if (err) {
+                      throw err;
+                    }
+                    done();
+                });
+        });
+    });
+
+    describe('/map failed', function() {
+        it('should return 200 from GET /map', function(done) {
+            request(server)
+                .get('/map')
+                .end((err, res) => {
+                    res.status.should.be.equal(200);
+                    if (err) {
+                      throw err;
+                    }
+                    done();
+                });
+        });
+    });
+});
 
 describe('Core controller unit tests:', function() {
     describe('Loading the home-page', function() {
@@ -24,21 +54,6 @@ describe('Core controller unit tests:', function() {
                     res.text.should.match(/name="username"/);
                     res.text.should.match(/name="password"/);
                     res.text.should.match(/action="[/]"/);
-                    if (err) {
-                      throw err;
-                    }
-                    done();
-                });
-        });
-    });
-
-    describe('Login wrong password', function() {
-        it('should return 400 from GET /', function(done) {
-            request(server)
-                .post('/')
-                .send({username: "te@test.com", password: "tes432"})
-                .end((err, res) => {
-                    res.status.should.be.equal(302);
                     if (err) {
                       throw err;
                     }
@@ -84,4 +99,5 @@ describe('Core controller unit tests:', function() {
         done();
     });
 });
+
 /* eslint-disable no-unused-vars, no-undef */
